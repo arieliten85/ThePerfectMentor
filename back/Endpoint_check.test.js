584e45aa275f6058bba6fd4d5a9ecffb46a31fc6
Ariel Ferencak <88351452+arieliten85@users.noreply.github.com>
2023-05-16 17:05:01 -0300

    Update Endpoint_check.test.js

--- back/Endpoint_check.test.js
+++ back/Endpoint_check.test.js
@@ -3,26 +3,18 @@
  const app = require('./server');
  const api = supertest(app)
  
-
- 
  describe("Endpoint Check", ()=>{
- 
-   
+  
    test('It should return a 201 status code', async () => {
      const  response = await api.get("/api/user/stadistics")
      expect(response.statusCode).toBe(201);
-      
      });
-
+  
      test('It should return a Array', async () => {
       const  response = await api.get("/api/user/stadistics")
       expect(response.body).toBeInstanceOf(Array);
-       
-      });
- 
-   
- 
       
+      });
  })
  
    
